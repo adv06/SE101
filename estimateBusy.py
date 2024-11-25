9dd86c1ee05964e7c13183dde2978fb4ba1d199a
Sayan Dey <sayandey@Sayans-MacBook-Air.local>
2024-11-24 19:47:04 -0500

    added the one-hot encoding and data to the code

--- estimateBusy.py
+++ estimateBusy.py
@@ -1,16 +1,41 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
 
 DATA_LENGTH = 1000
-# weather
-x1_train = tf.constant([])
 
-# time of week (minutes)
-x2_train = tf.constant([])
+#loaded dataset from csv file
+df = pd.read_csv('tim_hortons_weather_time_people_dataset.csv')
 
-#of people at tim hortons
-y_train = tf.constant([])
+#set encoder
+encoder = OneHotEncoder(sparse=False)
+weather_encoded = encoder.fit_transform(df['Weather'].values.reshape(-1, 1))
+
+# Step 2: Time of Day 
+time_data = df['Time of Day (minutes)'].values
+time_data = time_data / (7 * 24 * 60)  # Normalize the time between 0 and 1
+
+# Step 3: Number of People 
+people_data = df['Number of People'].values
+
+# Step 4: Create input tensors for TensorFlow
+x1_train = tf.constant(weather_encoded, dtype=tf.float32)  # One-hot encoded weather data
+x2_train = tf.constant(time_data.reshape(-1, 1), dtype=tf.float32)  # Normalized time data
+y_train = tf.constant(people_data, dtype=tf.float32)  # Number of people
+
+
+# # weather
+
+# x1_train = tf.constant([])
+
+# # time of week (minutes)
+# x2_train = tf.constant([])
+
+# #of people at tim hortons
+# y_train = tf.constant([])
 
 model = keras.Sequential([
     # First hidden
